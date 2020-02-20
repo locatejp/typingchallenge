8c27191f1a39eef5095489d8af76ea181ec64632
locatejp <jpcarlin@mbpro.attlocal.net>
2020-02-19 19:19:07 -0500

    Updated port env var

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const path = require('path')
 const db = require('./models')
-let PORT = 3000
+let PORT = process.env.PORT || 3000
 
 
 //==========
